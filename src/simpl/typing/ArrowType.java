a489c4a409cf2a1fa34087470692cc0c355840e9
kevinkwl <lingkangwei.kevin@gmail.com>
2016-11-11 18:23:10 +0800

    Type checking

--- src/simpl/typing/ArrowType.java
+++ src/simpl/typing/ArrowType.java
@@ -21,10 +21,9 @@ public final class ArrowType extends Type {
         }
         if (t instanceof ArrowType) {
             Substitution s1 = this.t1.unify(((ArrowType) t).t1);
-            this.t2 = s1.apply(this.t2);
-            ((ArrowType) t).t2 = s1.apply(((ArrowType) t).t2);
+            s1.apply(this.t2);
 
-            Substitution s2 = this.t2.unify(((ArrowType) t).t2);
+            Substitution s2 = s1.apply(this.t2).unify(s1.apply(((ArrowType) t).t2));
 
             return s2.compose(s1);
         }
--- src/simpl/typing/DefaultTypeEnv.java
+++ src/simpl/typing/DefaultTypeEnv.java
@@ -7,7 +7,38 @@ public class DefaultTypeEnv extends TypeEnv {
     private TypeEnv E;
 
     public DefaultTypeEnv() {
-        // TODO
+        Symbol fst = Symbol.symbol("fst");
+        Symbol snd = Symbol.symbol("snd");
+        Symbol hd = Symbol.symbol("hd");
+        Symbol tl = Symbol.symbol("tl");
+        Symbol iszero = Symbol.symbol("iszero");
+        Symbol pred = Symbol.symbol("pred");
+        Symbol succ = Symbol.symbol("succ");
+
+        TypeVar t = new TypeVar(false);
+        ArrowType fst_t = new ArrowType(new PairType(t, new TypeVar(false)), t);
+        E = TypeEnv.of(TypeEnv.empty, fst, fst_t);
+
+        t = new TypeVar(false);
+        ArrowType snd_t = new ArrowType(new PairType(new TypeVar(false), t), t);
+        E = TypeEnv.of(E, snd, snd_t);
+
+        t = new TypeVar(false);
+        ArrowType hd_t = new ArrowType(new ListType(t), t);
+        E = TypeEnv.of(E, hd, hd_t);
+
+        t = new TypeVar(false);
+        ListType tlist = new ListType(t);
+        ArrowType tl_t = new ArrowType(tlist, tlist);
+        E = TypeEnv.of(E, tl, tl_t);
+
+        ArrowType iszero_t = new ArrowType(Type.INT, Type.BOOL);
+        ArrowType pred_t = new ArrowType(Type.INT, Type.INT);
+        ArrowType succ_t = new ArrowType(Type.INT, Type.INT);
+        E = TypeEnv.of(E, iszero, iszero_t);
+        E = TypeEnv.of(E, pred, pred_t);
+        E = TypeEnv.of(E, succ, succ_t);
+
     }
 
     @Override
